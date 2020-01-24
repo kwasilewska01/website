391ea864e9247003f1936733ec80e055dd8683e6
kwasilewska01 <k.wasilewska01@gmail.com>
2020-01-24 19:05:47 +0100

    Updates

---
+++ precache-manifest.4cb62494cf02eae9eb234bf66ce3d9c2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433c7f6fe9ee077fd59d33a3a0fcb5bb",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "15560ad0e8ae03e9ba90",
+    "url": "/website/static/css/main.1d9df9f7.chunk.css"
+  },
+  {
+    "revision": "b57d1ac0c24dd2dce9d8",
+    "url": "/website/static/js/2.8a90f6cf.chunk.js"
+  },
+  {
+    "revision": "15560ad0e8ae03e9ba90",
+    "url": "/website/static/js/main.31288247.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "bc7d6bd13ac9a500aed0f10ab2138d35",
+    "url": "/website/static/media/arrow.bc7d6bd1.svg"
+  },
+  {
+    "revision": "2d176f62f665d7421db74d3a09d0ec1a",
+    "url": "/website/static/media/coworking.2d176f62.svg"
+  },
+  {
+    "revision": "3f64d73942f873d7124d88a893beb7ea",
+    "url": "/website/static/media/hiring.3f64d739.svg"
+  },
+  {
+    "revision": "10643f4e9921788db0cce2b63bf511d6",
+    "url": "/website/static/media/interview.10643f4e.svg"
+  },
+  {
+    "revision": "dbff3bb91e71d657794cf6141db87ef9",
+    "url": "/website/static/media/mailbox.dbff3bb9.svg"
+  },
+  {
+    "revision": "1ea6e58651f39ab811c9990b42269993",
+    "url": "/website/static/media/percentages.1ea6e586.svg"
+  },
+  {
+    "revision": "7e1b362a45cc6abe9f56c70b68f55ef9",
+    "url": "/website/static/media/setupwizard.7e1b362a.svg"
+  },
+  {
+    "revision": "ba3e0ca4fa215e570ab1537046480b50",
+    "url": "/website/static/media/spade.ba3e0ca4.svg"
+  }
+]);
